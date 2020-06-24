40a8db261b0be1efbaaf124afc6fb8199f14834f
Maddy <66322079+Mudassirwaseem23@users.noreply.github.com>
2020-06-24 17:23:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function getNumber(num){
+
+var result = document.getElementById("result")
+result.value += num;
+}
+
+function clearValue(){
+    var result = document.getElementById("result")
+result.value = "";
+}
+
+function resultvalue(){
+    var result = document.getElementById("result")
+
+result.value = eval(result.value)
+
+}
